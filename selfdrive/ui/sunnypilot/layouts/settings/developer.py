8ba48d838611a27d90b93c6d0a3af7b4f4cdcac9
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-05-24 11:36:21 -0400

    Add the toggle under advanced controls

--- selfdrive/ui/sunnypilot/layouts/settings/developer.py
+++ selfdrive/ui/sunnypilot/layouts/settings/developer.py
@@ -51,9 +51,13 @@ class DeveloperLayoutSP(DeveloperLayout):
 
     self.prebuilt_toggle = toggle_item_sp(tr("Quickboot Mode"), "", param="QuickBootToggle", callback=self._on_prebuilt_toggled)
 
+    self.konik_toggle = toggle_item_sp(tr("Use Konik API"), tr("Use Konik's API rather than comma's. Requires reboot."), param="KonikApi",
+                                       callback=self._on_konik_toggled)
+
     self.error_log_btn = button_item(tr("Error Log"), tr("VIEW"), tr("View the error log for sunnypilot crashes."), callback=self._on_error_log_clicked)
 
-    self.items: list = [self.show_advanced_controls, self.enable_github_runner_toggle, self.enable_copyparty_toggle, self.prebuilt_toggle, self.error_log_btn,]
+    self.items: list = [self.show_advanced_controls, self.enable_github_runner_toggle, self.enable_copyparty_toggle, \
+                        self.prebuilt_toggle, self.konik_toggle, self.error_log_btn,]
 
   @staticmethod
   def _on_prebuilt_toggled(state):
@@ -113,4 +117,5 @@ class DeveloperLayoutSP(DeveloperLayout):
 
     self.enable_copyparty_toggle.set_visible(show_advanced)
     self.enable_github_runner_toggle.set_visible(show_advanced and not self._is_release_branch)
+    self.konik_toggle.set_visible(show_advanced)
     self.error_log_btn.set_visible(not self._is_release_branch)
